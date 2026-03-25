006ed99c74a3de92eb9c1250dccd74ec037448ef
kiril <kiril.borysov.kb.2023@lpnu.ua>
2026-03-25 23:02:56 +0200

    kiril_1.03

--- b2b_dj/sellers/urls.py
+++ b2b_dj/sellers/urls.py
@@ -3,4 +3,11 @@ from . import views
 
 urlpatterns = [
     path("",views.main, name="main"),
+    path("seller_new_offers/", views.seller_new_offers, name="seller_new_offers"),
+    path("seller_product/", views.seller_product, name="seller_product"),
+    path("seller_order/", views.seller_order, name="seller_order"), 
+    path("seller_buyers/", views.seller_buyers, name="seller_buyers"),
+    path("seller_carriers/", views.seller_carriers, name="seller_carriers"),
+    path("seller_storage/", views.seller_storage, name="seller_storage"),
+    
 ]
--- b2b_dj/sellers/views.py
+++ b2b_dj/sellers/views.py
@@ -3,3 +3,21 @@ from django.shortcuts import render
 # Create your views here.
 def main(request):
     return render(request, "sellers/main.html")
+
+def seller_new_offers(request):
+    return render(request, "sellers/seller_new_offers.html")
+
+def seller_product(request):
+    return render(request, "sellers/seller_product.html")
+
+def seller_order(request):
+    return render(request, "sellers/seller_order.html")
+
+def seller_buyers(request):
+    return render(request, "sellers/seller_buyers.html")
+
+def seller_carriers(request):
+    return render(request, "sellers/seller_carriers.html")
+
+def seller_storage(request):
+    return render(request, "sellers/seller_storage.html")
