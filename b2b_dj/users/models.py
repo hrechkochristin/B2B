4f27fa28833873d5d7dd70f5f2167e496c5d5f60
Eridana <hrechkochristin@gmail.com>
2026-03-26 03:26:39 +0200

    Eridana_1.07 (UserDatabase model and 10 fantom users added)

---
+++ b2b_dj/users/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
+class User(AbstractBaseUser):
+    name = models.CharField(max_length=255, default="Eridana") 
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=20, blank=True)
+    address = models.TextField(blank=True)
+
+    is_seller = models.BooleanField(default=False)
+    is_buyer = models.BooleanField(default=False)
+    is_carrier = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.username
