e6ab3b9280940281b230591214d6fe53f7728172
Eridana <hrechkochristin@gmail.com>
2026-03-28 02:39:10 +0200

    Eridana_1.12 (Orders Database updated, OrderItem Database added)

--- b2b_dj/orders/models.py
+++ b2b_dj/orders/models.py
@@ -31,8 +31,6 @@ class Order(models.Model):
         related_name='orders'
     )
 
-    price = models.DecimalField(max_digits=10, decimal_places=2)    
-
     # 📍 маршрут
     pickup_address = models.CharField(max_length=255)
     delivery_address = models.CharField(max_length=255)
@@ -63,6 +61,17 @@ class Order(models.Model):
 
     def total_volume(self):
         return sum(p.total_volume() for p in self.products.all())
+    
+    def total_price(self):
+        return sum(item.price * item.quantity for item in self.items.all())
 
     def __str__(self):
         return f"Order {self.id}"
+    
+
+class OrderItem(models.Model):
+    order = models.ForeignKey('Order', on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey('products.Product', on_delete=models.CASCADE)
+
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
